8e6600c5477b4730dfee3ae0e78c6e16c83e3a28
heshamabdelhamead <34060487+heshamabdelhamead@users.noreply.github.com>
2025-09-30 15:34:56 +0300

    intail commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SignUp
 signup with swiftUI , combine , MVVM
+this is a demo project for using latest apple technologies 
+## Installation 
+ 1. Clone the repo:
+   https://github.com/heshamabdelhamead/SignUp.git
+2. Run the project on xcode . 
+
---
+++ SignUp/SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by hesham abd elhamead on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
