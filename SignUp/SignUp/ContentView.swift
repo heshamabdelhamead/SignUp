058efe4e533041cad6578d8f715da4fc5d5993ba
heshamabdelhamead <34060487+heshamabdelhamead@users.noreply.github.com>
2025-09-30 19:18:14 +0300

    adding ui

--- SignUp/SignUp/ContentView.swift
+++ SignUp/SignUp/ContentView.swift
@@ -8,17 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userName : String = ""
+    var userNameError : String = "Error"
+    @State var email : String = ""
+    var emailError : String = "Error"
+    @State var password : String = ""
+    var passwordError : String = "Error"
+    @State var ConfirmPassword : String = ""
+    var ConfirmPasswordError : String = "Error"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.green.edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Sign up ")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                CredentialsView(title: $userName, placeHolder: "User Name", errorText: userNameError)
+                CredentialsView(title: $email, placeHolder: "email", errorText: emailError, keyboardType: .emailAddress)
+                CredentialsView(title: $password, placeHolder: "password", errorText: emailError, isSecure: true)
+                CredentialsView(title: $ConfirmPassword, placeHolder: "confirm password", errorText: emailError, isSecure: true)
+                
+                Button(" Sign up") {
+                    print("hi")
+                }
+                .frame(minWidth: 0.0,  maxWidth: .infinity)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .clipShape(.capsule)
+               // .buttonBorderShape(.buttonBorder)
+            }
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CredentialsView: View {
+    @Binding var  title : String
+    var placeHolder : String
+    var errorText : String
+    var keyboardType : UIKeyboardType = .default
+    var isSecure : Bool = false
+   var body: some View {
+       VStack{
+           if isSecure {
+               SecureField(placeHolder, text: $title)
+                   .keyboardType(keyboardType)
+               
+                   .frame(height: 20)
+                   .padding()
+                   .background(Color.white)
+                   .clipShape(.buttonBorder)
+           }else {
+               TextField( placeHolder, text: $title)
+               
+                   .keyboardType(keyboardType)
+               
+                   .frame(height: 20)
+                   .padding()
+                   .background(Color.white)
+                   .clipShape(.buttonBorder)
+           }
+           Text(errorText)
+               .foregroundStyle(.red)
+               .frame(minWidth: 0, maxWidth: .infinity , alignment: .trailing)
+               .fontWeight(.medium)
+        
+        
+       }
+    }
+    
+    
 }
